5453230a6063ec2fec9386e2c16ef92d7d021312
lamkostal <lamkostal@gmail.com>
2024-12-17 17:25:43 +0200

    preference:'light'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr:true,
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  colorMode:{
+    preference:'light'
+  }
 })
